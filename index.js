d8aa9173cb32b343594ce256ac8afe1011fa4389
arepallyravi <118881183+arepallyravi@users.noreply.github.com>
2022-12-12 17:29:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("hello learning node")
+
+function add(a,b){
+    console.log("sum of two number =",a+b)
+}
+
+add(3,4)
+
+function subtract(a,b){
+    console.log("result=",a-b)
+}
+
+
+subtract(6,2)
+
