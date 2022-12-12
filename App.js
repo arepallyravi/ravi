2464ac2c2e6009266d7bca53a5462dfec6aa84db
arepallyravi <118881183+arepallyravi@users.noreply.github.com>
2022-12-12 13:38:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css'
+import EmployeeFn from './EmployeeFn';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>hello world</h1>
+       <EmployeeFn/>
+       
+    </div>
+  );
+}
+
+export default App;
